80d5e94ff30f664d2bd10d9aedc9b45b84f305e9
Sheldon <sheldonaldridge@yahoo.com>
2022-03-15 20:43:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 let userInput = document.getElementById("num");
 let submit = document.getElementById("submit").onclick;
 let answer;
+let odd;
+let even;
 
+function submitbtn() {
+  answer = userInput.value;
+  document.getElementById("result").innerHTML = answer;
 
-function submitbtn()
-{
-    answer = userInput.value;
-    document.getElementById("result").innerHTML = answer;
+  if (answer % 2 == 0) {
+    even = " Is an even number";
+    document.getElementById("result").innerHTML = answer + even;
+  } else {
+    odd = " Is an odd number";
+    document.getElementById("result").innerHTML = answer + odd;
+  }
 }
